7138aa5cb2bed46fd32316280d0de898c4893adf
unknown <bachinge@technikum-wien.at>
2018-10-17 21:15:41 +0200

    3.2.18 WIP

--- TermServer/src/java/de/fhdo/terminologie/Definitions.java
+++ TermServer/src/java/de/fhdo/terminologie/Definitions.java
@@ -32,7 +32,7 @@ public class Definitions
   public final static String APP_KEY = "TERMSERVER";
 
     public static String getSwVersion() {
-        return "3.2.17";
+        return "3.2.18";
     }
 
   public static enum STATUS_CODES
