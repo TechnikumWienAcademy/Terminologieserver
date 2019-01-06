081ee3153c589d72d261377dcd2fc4e2487c962c
unknown <bachinge@technikum-wien.at>
2019-01-06 09:55:16 +0100

    3.2.25

--- TermServer/src/java/de/fhdo/terminologie/Definitions.java
+++ TermServer/src/java/de/fhdo/terminologie/Definitions.java
@@ -32,7 +32,7 @@ public class Definitions
   public final static String APP_KEY = "TERMSERVER";
 
     public static String getSwVersion() {
-        return "3.2.24";
+        return "3.2.25";
     }
 
   public static enum STATUS_CODES
